375e006c4d7a66229b55cc4043af83cdf680d61a
Saskya Irena <saskya.irena@g.hde.co.jp>
2014-06-10 12:42:21 +0900

    Editting migration issues from GAE

--- app/views.py
+++ app/views.py
@@ -2,18 +2,18 @@ from flask import render_template, request, redirect, url_for, current_app, Resp
 from app import app
 from forms import TweetForm
 #from tweetings import Tweet
-from retrieve import Retrieve
+#from retrieve import Retrieve
 #from google.appengine.ext import db
 #from google.appengine.api import images
 import datetime
-from tweetdisplay import TweetDisplay
+#from tweetdisplay import TweetDisplay
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 	form = TweetForm(request.form)
-	tweetmaster = Tweet(key_name='tm')
-	t = Tweet(parent=tweetmaster)
+	#tweetmaster = Tweet(key_name='tm')
+	#t = Tweet(parent=tweetmaster)
 	
 	if request.method=='POST' and form.validate():
 		t.content = request.form['tweet']
@@ -21,31 +21,32 @@ def index():
 		file = request.files['file']
 		filedata=file.read()
 		
-		if file:
-			t.image=db.Blob(filedata)
-		else:
-			t.image = None
+		#if file:
+		#	t.image=db.Blob(filedata)
+		#else:
+		#	t.image = None
 
-		t.put()
-		return redirect(url_for('index'))	
-	retrieve = Tweet.all()
-	retrieve.order("-date")
+		#t.put()
+		#return redirect(url_for('index'))	
+	retrieve = []
+    #retrieve = Tweet.all()
+	#retrieve.order("-date")
 	photo_url = {}
 	display = []
-	retrieve.ancestor(tweetmaster)
-	for member in retrieve:
-		d = TweetDisplay()
-		photo_link = url_for(".show", key=member.key())
-		d.keyd = str(member.key())
-		if member.image is None:
-			d.image_link = None
-		else:
-			d.image_link = photo_link
-		d.content = member.content
-		d.date = member.date
-		display.append(d)
+	#retrieve.ancestor(tweetmaster)
+	#for member in retrieve:
+		#d = TweetDisplay()
+		#photo_link = url_for(".show", key=member.key())
+		#d.keyd = str(member.key())
+		#if member.image is None:
+		#	d.image_link = None
+		#else:
+		#	d.image_link = photo_link
+		#d.content = member.content
+		#d.date = member.date
+		#display.append(d)
 	
-	return render_template("index.html", form=form, display=display, retrieve=retrieve)
+	return render_template("index.html", form=form)
 	
 @app.route('/show/<key>', methods=["GET"])
 def show(key):
